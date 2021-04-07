2df65844a41a60bf1aa2be216eb1d859c6262c65
Nisa Delgado <nisadelgado@gmail.com>
2021-04-07 00:27:23 -0400

    Update helpers.php

--- app/helpers.php
+++ app/helpers.php
@@ -2,14 +2,7 @@
 
 function description($text)
 {
-	$text = preg_replace("/((http|https|www)[^\s]+)/", '<a style="color: #858796" target="_blank" href="$1">$0</a>', $text);
-	$text = preg_replace("/href=\"www/", 'href="http://www', $text);
-	
-	$text = str_replace('[', '<img class="img-fluid" src="/resources/assets/files/', $text);
-	$text = str_replace(']', '">', $text);
-
-	$text = nl2br($text);
-
+	$text = Michelf\MarkdownExtra::defaultTransform($text);
 	return $text;
 }
 
