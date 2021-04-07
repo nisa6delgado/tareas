22a2a5925b64025322ec22a53a8d48181271201b
Nisa Delgado <nisadelgado@gmail.com>
2021-04-07 00:22:03 -0400

    Markdown en descripción de tareas

--- resources/views/dashboard/index.php
+++ resources/views/dashboard/index.php
@@ -40,7 +40,7 @@
 						<div class="card-body">
 							<p><?php echo description($task->description); ?></p>
 
-							<?php if ($task->files->count() && (!strpos($task->description, '[') && !strpos($task->description, ']'))): ?>
+							<?php if ($task->files->count() && !strpos($task->description, '![')): ?>
 								<hr>
 								<b>Archivos adjuntos</b>
 								<br><br>
