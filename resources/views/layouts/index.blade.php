5004f0b3d4e59724f1b0aa00b37cf3728724416b
Nisa Delgado <nisadelgado@gmail.com>
2021-05-12 10:06:05 -0400

    Año de los derechos reservados actualizado

--- resources/views/layouts/index.blade.php
+++ resources/views/layouts/index.blade.php
@@ -135,7 +135,7 @@
 			<footer class="sticky-footer bg-white">
 				<div class="container my-auto">
 					<div class="copyright text-center my-auto">
-						<span>Todos los derechos reservados &copy; Nisa Delgado 2020</span>
+						<span>Todos los derechos reservados &copy; Nisa Delgado 2021</span>
 					</div>
 				</div>
 			</footer>
