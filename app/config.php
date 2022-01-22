7cb2a2f118e4299a7be5ef21bd2c17f3fd6b22cb
Nisa Delgado <nisadelgado@gmail.com>
2022-01-22 14:28:00 -0400

    Framework actualizado

--- app/config.php
+++ app/config.php
@@ -3,7 +3,7 @@
 return [
 	// General
 	'application_name' 	=> 'Tareas',
-	'version'			=> '1.2.35',
+	'version'			=> '1.0.63',
 
 	// Region
 	'language' 			=> 'es',
@@ -13,6 +13,9 @@ return [
 	// Environment
 	'environment' 		=> 'production',
 	'errors' 			=> true,
+
+    // Redirect after login
+    'redirect_login'    => '/dashboard',
 
 	// Database.
 	'database' 		=> [
@@ -26,15 +29,24 @@ return [
 		]
 	],
 
-	// Facebook login
-	'facebook'			=> [
-		'app_id' 		=> '',
-		'app_secret' 	=> ''
-	],
+	// Needed to send emails locally.
+    'smtp'              => [
+        'host'          => '',
+        'username'      => '',
+        'password'      => '',
+        'port'          => ''
+    ],
 
-	// Google login
-	'google'			=> [
-		'client_id' 	=> '',
-		'client_secret' => ''
-	],
+    // Login with social networks
+    'google' => [
+        'client_id'     => '',
+        'client_secret' => '',
+        'redirect'      => ''
+    ],
+
+    'facebook' => [
+        'app_id'        => '',
+        'app_secret'    => '',
+        'redirect'      => ''
+    ]
 ];
