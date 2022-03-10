5693762fc4ff4ea741c096e9814488971e2f5df3
Nisa Delgado <nisadelgado@gmail.com>
2022-03-10 10:37:26 -0400

    Botones cuadrados

--- resources/views/components/link.blade.php
+++ resources/views/components/link.blade.php
@@ -1,5 +1,5 @@
 <div class="mr-1">
-    <a data-tooltip-target="{{ $id }}" {!! isset($delete) ? 'x-on:click="confirmDelete(event, $el)"' : '' !!} href="{{ $href }}" class="{{ $class }} shadow bg-{{ $color }}-300 hover:bg-{{ $color }}-500 focus:shadow-outline focus:outline-none text-white font-bold py-1 px-2 rounded">
+    <a data-tooltip-target="{{ $id }}" {!! isset($delete) ? 'x-on:click="confirmDelete(event, $el)"' : '' !!} href="{{ $href }}" class="{{ $class }} shadow bg-{{ $color }}-300 hover:bg-{{ $color }}-500 focus:shadow-outline focus:outline-none text-white font-bold py-1 px-2">
         <i class="{{ $icon }}"></i>
     </a>
 
