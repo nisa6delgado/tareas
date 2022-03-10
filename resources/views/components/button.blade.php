eb7ec558b043f1ef31eeaf935f00f4aaacfe6f90
Nisa Delgado <nisadelgado@gmail.com>
2022-03-10 10:37:26 -0400

    Botones cuadrados

--- resources/views/components/button.blade.php
+++ resources/views/components/button.blade.php
@@ -1,7 +1,7 @@
 <div class="md:flex md:items-center">
     <div class="md:w-1/3"></div>
     <div class="md:w-2/3">
-        <button class="{{ $class }} shadow bg-black hover:bg-black focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded" type="submit">
+        <button class="{{ $class }} shadow bg-black hover:bg-black focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4" type="submit">
             {{ isset($text) ? $text : 'Guardar' }}
         </button>
     </div>
