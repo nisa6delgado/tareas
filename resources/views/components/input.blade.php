66046d9b3f102b5cec4a210a2f7e3a61e4588224
Nisa Delgado <nisadelgado@gmail.com>
2022-03-21 11:24:18 -0400

    Mostrar campo de contraseña vacío en configuración

--- resources/views/components/input.blade.php
+++ resources/views/components/input.blade.php
@@ -5,6 +5,6 @@
         </label>
     </div>
     <div class="md:w-2/3">
-        <input autofocus class="form-input block w-full focus:bg-white" id="{{ $key }}" name="{{ $key }}" type="{{ isset($password) ? 'password' : 'text' }}" {{ isset($required) ? 'required' : '' }} value="{{ $value }}">
+        <input autofocus class="form-input block w-full focus:bg-white" id="{{ $key }}" name="{{ $key }}" type="{{ isset($password) ? 'password' : 'text' }}" {{ isset($required) ? 'required' : '' }} value="{{ !isset($password) ? $value : '' }}">
     </div>
 </div>
