f98d8a3a9277465f4dbb26c03755e98b8dd224c9
Nisa Delgado <nisadelgado@gmail.com>
2022-07-19 08:32:36 -0400

    Sufijo en controladores

--- app/routes.php
+++ app/routes.php
@@ -14,38 +14,38 @@
 $route->auth();
 
 // Home
-$route->get('/', [Home::class, 'index']);
+$route->get('/', [HomeController::class, 'index']);
 
-// Configurations
-$route->get('/configurations', [Configurations::class, 'edit']);
-$route->post('/configurations', [Configurations::class, 'update']);
+// Configuration
+$route->get('/configurations', [ConfigurationController::class, 'edit']);
+$route->post('/configurations', [ConfigurationController::class, 'update']);
 
-// Projects
-$route->get('/projects/create', [Projects::class, 'create']);
-$route->post('/projects/store', [Projects::class, 'store']);
-$route->post('/projects/update', [Projects::class, 'update']);
+// Project
+$route->get('/projects/create', [ProjectController::class, 'create']);
+$route->post('/projects/store', [ProjectController::class, 'store']);
+$route->post('/projects/update', [ProjectController::class, 'update']);
 
-$route->get('/projects/show/{slug}', [Projects::class, 'show']);
-$route->get('/projects/edit/{slug}', [Projects::class, 'edit']);
-$route->get('/projects/delete/{slug}', [Projects::class, 'delete']);
+$route->get('/projects/show/{slug}', [ProjectController::class, 'show']);
+$route->get('/projects/edit/{slug}', [ProjectController::class, 'edit']);
+$route->get('/projects/delete/{slug}', [ProjectController::class, 'delete']);
 
-// Tasks
-$route->get('/tasks/create/{slug}', [Tasks::class, 'create']);
-$route->post('/tasks/store', [Tasks::class, 'store']);
+// Task
+$route->get('/tasks/create/{slug}', [TaskController::class, 'create']);
+$route->post('/tasks/store', [TaskController::class, 'store']);
 
-$route->get('/tasks/show/{slug}/{id}', [Tasks::class, 'show']);
+$route->get('/tasks/show/{slug}/{id}', [TaskController::class, 'show']);
 
-$route->get('/tasks/edit/{slug}/{id}', [Tasks::class, 'edit']);
-$route->post('/tasks/update', [Tasks::class, 'update']);
+$route->get('/tasks/edit/{slug}/{id}', [TaskController::class, 'edit']);
+$route->post('/tasks/update', [TaskController::class, 'update']);
 
-$route->get('/tasks/delete/{slug}/{id}', [Tasks::class, 'delete']);
+$route->get('/tasks/delete/{slug}/{id}', [TaskController::class, 'delete']);
 
 // Status
-$route->get('/status/{id_task}/{status}', [Status::class, 'update']);
+$route->get('/status/{id_task}/{status}', [StatusController::class, 'update']);
 
-// Files
-$route->get('/files/show/{id}', [Files::class, 'show']);
-$route->get('/files/delete/{id}', [Files::class, 'delete']);
+// File
+$route->get('/files/show/{id}', [FileController::class, 'show']);
+$route->get('/files/delete/{id}', [FileController::class, 'delete']);
 
 // Backup
-$route->get('/backup', [Backups::class, 'generate']);
+$route->get('/backup', [BackupController::class, 'generate']);
