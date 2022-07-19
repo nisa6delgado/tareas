bd8042751fdb45dff8c5f97c9bf3852299ae8671
Nisa Delgado <nisadelgado@gmail.com>
2022-07-19 08:33:00 -0400

    Sufijo en controladores

---
+++ app/Controllers/AuthController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Models\Configuration;
+use View;
+use Redirect;
+
+class AuthController extends Controller
+{
+    /**
+     * Display a login page.
+     *
+     * @return View
+     */
+    public function index(): View
+    {
+        return view('auth.login');
+    }
+
+    /**
+     * Login user.
+     *
+     * @return Redirect
+     */
+    public function login(): Redirect
+    {
+        $user       = Configuration::where('key', 'user')->first()->value;
+        $password   = Configuration::where('key', 'password')->first()->value;
+
+        if ($user == request('user') && $password == encrypt(request('password'))) {
+            session('authenticate', 1);
+            return redirect('/');
+        }
+
+        return redirect('/login')->with('error', 'Datos incorrectos');
+    }
+
+    /**
+     * Logout user.
+     *
+     * @return Redirect
+     */
+    public function logout(): Redirect
+    {
+        logout();
+        return redirect('/login');
+    }
+}
---
+++ app/Controllers/StatusController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Models\Task;
+use Redirect;
+
+class StatusController extends Controller
+{
+    /**
+     * Verify if user is logged.
+     *
+     * @return auth
+     */
+    public function __construct()
+    {
+        $this->middleware('Auth');
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  int      $id_task
+     * @param  string   $status
+     * @return Redirect
+     */
+    public function update(int $id_task, string $status): Redirect
+    {
+        $task = Task::find($id_task);
+        $status = ($status == 'done') ? 1 : 0;
+        $task->update([
+            'status'        => $status,
+            'date_update'   => now('Y-m-d H:i:s')
+        ]);
+
+        return redirect('/tasks/show/' . $task->project->slug . '/' . $id_task);
+    }
+}
