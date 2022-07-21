0c4bc3061b187ca25e21a022bdc9c04b73101121
Nisa Delgado <nisadelgado@gmail.com>
2022-07-21 08:30:36 -0400

    Validaciones de backend para proyectos

---
+++ app/Validations/ProjectStoreValidation.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Validations;
+
+class ProjectStoreValidation extends Validation
+{
+	/**
+	* Get the validation rules that apply to the request.
+	*
+	* @return array
+	*/
+	public function rules(): array
+	{
+		return [
+			'name'  => 'required|unique:projects',
+			'icon'  => 'required',
+			'color' => 'required',
+		];
+	}
+
+	/**
+	 * Get the error messages for the defined validation rules.
+	 *
+	 * @return array
+	 */
+	public function messages(): array
+	{
+		return [
+			'name.required'  => 'El nombre del proyecto es obligatorio.',
+			'name.unique'    => 'El nombre del proyecto ya existe.',
+			'icon.required'  => 'El ícono del proyecto es obligatorio.',
+			'color.required' => 'El color del proyecto es obligatorio.'
+		];
+	}
+}
--- resources/views/projects/create.blade.php
+++ resources/views/projects/create.blade.php
@@ -5,6 +5,8 @@
 
         <hr class="bg-gray-300 my-6">
 
+        <x-validation-errors/>
+
         <div class="p-8 mt-6 m-1 lg:mt-0 leading-normal rounded shadow bg-white">
             <form method="POST" action="/projects/store">
                 <x-input required label="Nombre" key="name" value=""/>
