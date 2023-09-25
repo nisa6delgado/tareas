84435945c2751cce9f00e7198fa44c89a0b88515
Nisa Delgado <nisadelgado@gmail.com>
2023-09-25 13:21:59 -0400

    Editor HTML

--- resources/views/tasks/show.blade.php
+++ resources/views/tasks/show.blade.php
@@ -79,4 +79,25 @@
             </div>
         @endif
     </section>
+
+    @if($task->format == 'html')
+        <x-slot:js>
+            <script src="{{ node('tinymce/tinymce.js') }}"></script>
+
+            <script>
+                tinymce.init({
+                    selector: 'textarea',
+                    plugins: 'ai tinycomments mentions anchor autolink charmap codesample emoticons image link lists media searchreplace table visualblocks wordcount checklist mediaembed casechange export formatpainter pageembed permanentpen footnotes advtemplate advtable advcode editimage tableofcontents mergetags powerpaste tinymcespellchecker autocorrect a11ychecker typography inlinecss',
+                    toolbar: 'undo redo | blocks fontfamily fontsize | bold italic underline strikethrough | link image media table mergetags | align lineheight | tinycomments | checklist numlist bullist indent outdent | emoticons charmap | removeformat',
+                    tinycomments_mode: 'embedded',
+                    tinycomments_author: 'Author name',
+                    mergetags_list: [
+                        { value: 'First.Name', title: 'First Name' },
+                        { value: 'Email', title: 'Email' },
+                    ],
+                    ai_request: (request, respondWith) => respondWith.string(() => Promise.reject("See docs to implement AI Assistant"))
+                });
+            </script>
+        </x-slot:js>
+    @endif
 </x-template>
