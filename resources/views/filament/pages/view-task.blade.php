77a5d1512942b3dc5c3bb301d1be3716f30e1af9
Nisa Delgado <nisadelgado@gmail.com>
2025-02-23 09:57:45 -0400

    Desplazamiento vertical en contenedor de descripción de tarreas

--- resources/views/filament/pages/view-task.blade.php
+++ resources/views/filament/pages/view-task.blade.php
@@ -1,6 +1,6 @@
 <x-filament-panels::page>
     @if($task->description || $task->files)
-        <div class="bg-white rounded shadow-sm p-30 dark:bg-gray-900" style="padding: 30px">
+        <div class="description-task bg-white rounded shadow-sm p-30 dark:bg-gray-900">
             @if($task->format == 'checklist')
                 {{ checklist($task->description) }}
             @endif
