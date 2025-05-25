8b7a828f3681169a7b98aed9c6c9db0d46f8a2fb
nisa6delgado <nisadelgado@gmail.com>
2025-05-25 15:01:31 -0400

    Comando para optimizar tabla de actividad

---
+++ app/Console/Commands/OptimizeActivityTable.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\User;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\DB;
+
+class OptimizeActivityTable extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:optimize-activity-table';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Delete old user activity logs.';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $users = User::get();
+
+        foreach ($users as $user) {
+            $ids = DB::table('activity_log')
+                ->where('user_id', $user->id)
+                ->orderByDesc('id')
+                ->skip(1000)
+                ->pluck('id');
+
+            DB::table('activity_log')
+                ->whereIn('id', $ids)
+                ->delete();
+        }
+    }
+}
